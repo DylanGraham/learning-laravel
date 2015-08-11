953ad2583adde0404a54557b6a991d04162da013
Dylan Graham <dylan.graham@gmail.com>
2015-08-11 12:51:11 +1000

    Initial commit

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,19 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller {
+
+    public function about()
+    {
+        $data = [
+            'first' => 'Dylan',
+            'last'  => 'Woot',
+        ];
+
+        return view('pages.about', compact('data'));
+    }
+}
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('app')
+
+@section('content')
+    <h1>ABOUT {{ $data['first'] }} {{ $data['last'] }}</h1>
+@stop
