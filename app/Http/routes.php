92b011d34d7d8150f29d428c738e3baccef3e601
Dylan Graham <dylan.graham@gmail.com>
2015-08-13 16:40:52 +1000

    End lesson 10

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -3,8 +3,11 @@
     Route::get('about', 'PagesController@about');
     Route::get('contact', 'PagesController@contact');
     Route::get('articles', 'ArticlesController@index');
+    Route::get('articles/create', 'ArticlesController@create');
     Route::get('articles/{id}', 'ArticlesController@show');
+    Route::post('articles', 'ArticlesController@store');
 
-    Route::get('foo', function() {
+/*    Route::get('foo', function() {
         return 'Bar';
     });
+*/
